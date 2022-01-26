eb759d8aaab3f09f20db6a1e3d3b300671c02db5
0x01FF <33737928+0x01FF@users.noreply.github.com>
2022-01-26 13:55:20 -0600

    Add files via upload
    added new configparser system, allows for setting changes to carry over between uses of the script.

--- File_Sharer.py
+++ File_Sharer.py
@@ -1,25 +1,28 @@
-import socket, os
+import socket, os, configparser
 
 SEPARATOR = "<SEPARATOR>"
 BUFFER_SIZE = 4096
-host = '127.0.0.1'
-port = 6969
+config = configparser.ConfigParser()
+config.read('config.ini')
+host = config['SETTINGS']['HOST']
+port = int(config['SETTINGS']['PORT'])
 
 
 while True:
 	print('> ',end='')
-	user_input = input()
-	if user_input.lower() == "exit":
+	user_input = input().lower()
+	if user_input == "exit":
 		exit()
-	elif user_input.lower() == "help":
+	elif user_input == "help":
 		print("""
 		EXIT  :  exit the program
 		HELP  :  bring up this menu
 		SHARE {PATH and FILE NAME}  : share filename with set host
 		SET {OPTION} {INPUT}  : options are PORT and HOST
 		SERVER  :  set client to server mode (must use ctrl + c to end)
+		PRINT {OPTION}  :  options are PORT and HOST
 		""")
-	elif "share" in user_input.lower():
+	elif "share" in user_input:
 		args = user_input.split()
 		if os.path.exists(args[1]):
 			s = socket.socket()
@@ -39,23 +42,34 @@ while True:
 			s.close()
 		else:
 			print(f'{args[1]} does not exist.')
-	elif "set" in user_input.lower():
+	elif "set" in user_input:
 		trigger = False
 		args = user_input.split()
-		if args[1].lower() == "host" and args[2]:
-			trigger = True
-			host = args[2]
-		elif args[1].lower() == "port" and args[2]:
-			if args[2].isnumeric():
-				port = int(args[2])
-				trigger = True
-			else:
-				print("That is not a valid port number.")
+		if len(args) == 1:
+			print('Not enough arguments, check HELP')
 		else:
-			print("Syntax Error, refer to HELP.")
-		if trigger:
-			print(f'{args[1].title()} was set to {args[2]}.')
-	elif user_input.lower() == 'server':
+			if args[1] == "host" and args[2]:
+				trigger = True
+				host = args[2]
+				config_write = configparser.ConfigParser()
+				config_write['SETTINGS'] = {'HOST' : args[2], "PORT" : port}
+				with open('config.ini', 'w') as config_file:
+					config_write.write(config_file)
+			elif args[1] == "port" and args[2]:
+				if args[2].isnumeric():
+					port = int(args[2])
+					trigger = True
+					config_write = configparser.ConfigParser()
+					config_write['SETTINGS'] = {'HOST' : host, "PORT" : args[2]}
+					with open('config.ini', 'w') as config_file:
+						config_write.write(config_file)
+				else:
+					print("That is not a valid port number.")
+			else:
+				print("Syntax Error, refer to HELP.")
+			if trigger:
+				print(f'{args[1].title()} was set to {args[2]}.')
+	elif user_input == 'server':
 		s = socket.socket()
 		while True:
 			s.bind((host, port))
@@ -74,5 +88,16 @@ while True:
 					f.write(bytes_read)
 			conn.close()
 			s.close()
+	elif "print" in user_input:
+		args = user_input.split()
+		if len(args) == 1:
+			print('Not enough arguments, check HELP')
+		else:
+			if args[1] == 'host':
+				print(host)
+			elif args[1] == 'port':
+				print(port)
+			else:
+				print("That is not a valid option, please refer to HELP.")
 	else:
 		print(f"{user_input} is not a command. Maybe reference the HELP command?")
